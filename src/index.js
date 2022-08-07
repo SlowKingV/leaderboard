945e84db43dd056456d0530bbc8441a3a11c30b5
Diego Luna <slowkingvii@gmail.com>
2022-08-06 19:12:00 -0500

    Add necessary styling

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
 // Import Assets
+import '@fortawesome/fontawesome-free/js/fontawesome.js';
+import '@fortawesome/fontawesome-free/js/solid.js';
 import './style.css';
 
 // Import modules
 import ScoreList from './modules/ScoreList.js';
-import updateScores from './modules/DOMFunctions.js';
+import { calcWidth, updateScores } from './modules/DOMFunctions.js';
 import { postScore } from './modules/APIConsumer.js';
 
 // Create new Instance of ScoreList
 const scoresContainer = document.getElementById('score-list');
 const scoreList = new ScoreList(scoresContainer);
 
+const setGraph = () => {
+  const maxScore = scoreList.list[0].score;
+  const listItems = Array.from(scoresContainer.children);
+
+  listItems.forEach((item, index) => {
+    item.children[0].style.width = `${calcWidth(scoreList.list[index].score, maxScore)}%`;
+  });
+};
+
 // Update scores on page load
-window.onload = () => {
-  updateScores(scoreList);
+window.onload = async () => {
+  await updateScores(scoreList);
+  setGraph();
 };
 
 // Update scores when clicking on the 'Refresh' button
 const refreshBtn = document.getElementById('refresh');
-refreshBtn.addEventListener('click', () => { updateScores(scoreList); });
+refreshBtn.addEventListener('click', async () => {
+  await updateScores(scoreList);
+  setGraph();
+});
 
 // Post data to API on form submit
 const userInput = document.getElementById('user-input');
--- src/modules/DOMFunctions.js
+++ src/modules/DOMFunctions.js
@@ -3,14 +3,30 @@ import { fetchScores } from './APIConsumer.js';
 const updateHTML = (container, scores) => {
   container.innerHTML = '';
 
-  scores.forEach((entry) => {
+  scores.forEach((entry, index) => {
     const listItem = document.createElement('li');
-    listItem.textContent = `${entry.user}: ${entry.score}`;
+    const content = [
+      document.createElement('div'),
+      document.createElement('span'),
+      document.createElement('span'),
+      document.createElement('span'),
+    ];
+
+    content[0].setAttribute('class', 'graph');
+    content[1].setAttribute('class', 'position');
+    content[1].textContent = `${index + 1}. `;
+    content[2].setAttribute('class', 'user');
+    content[2].textContent = entry.user;
+    content[3].setAttribute('class', 'score');
+    content[3].textContent = entry.score;
+    listItem.append(...content);
     container.appendChild(listItem);
   });
 };
 
-export default async (scoreList) => {
+export const calcWidth = (score, maxScore) => (score * 100) / maxScore;
+
+export const updateScores = async (scoreList) => {
   scoreList.list = await fetchScores();
   updateHTML(scoreList.container, scoreList.list);
 };
